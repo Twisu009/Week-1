ced58fa376ab0a0bbc3308f9348933663256bde1
Twisu009 <prayushag@gmail.com>
2023-10-09 19:42:03 +0545

    initializing report

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import HelloReact from './HelloReact';
+
+function App() {
+  return (
+
+    <div className="App">
+        <p>
+          <HelloReact/>
+        </p>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/HelloReact.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+//import { Modal } from 'antd';
+
+const HelloReact: React.FC = () => {
+    return(
+        <div>
+           <p style={{fontSize: '50px', color: 'green'}}> 
+           
+           <b>Hello, React!</b>
+
+           </p>
+        </div>
+    );
+
+};
+
+export default HelloReact;
+
